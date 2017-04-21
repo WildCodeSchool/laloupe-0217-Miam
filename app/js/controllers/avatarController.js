ae6f630b29d366415815f127d9e7272de9e21b95
peche56 <c.filleul56@gmail.com>
2017-04-21 11:01:17 +0200

    avatar page without css

---
+++ app/js/controllers/avatarController.js
@@ -0,0 +1,36 @@
+angular.module('app')
+    .controller('AvatarController', function($scope) {
+        var i = 0;
+
+        var avatars = [
+            {
+                1: '/img/avatar.jpg',
+                2: '/img/avatar1.2.jpg'
+            },
+            {
+                1: '/img/avatar2.jpg',
+                2: '/img/avatar1.2.jpg'
+            }
+        ];
+
+        $scope.prev = function() {
+            i--;
+
+            if (i < 0) {
+                i = avatars.length - 1;
+            }
+            $scope.currentAvatar = avatars[i][1];
+        };
+
+        $scope.next = function() {
+            i++;
+
+            if (i >= avatars.length) {
+                i = 0;
+            }
+            $scope.currentAvatar = avatars[i][1];
+        };
+
+        $scope.currentAvatar = avatars[i][1];
+
+    });
