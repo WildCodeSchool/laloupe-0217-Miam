ba491907b124050c2ce99f5a244b66b310a13acd
peche56 <c.filleul56@gmail.com>
2017-04-25 13:34:26 +0200

    service ok start controller

--- app/js/controllers/mangerController.js
+++ app/js/controllers/mangerController.js
@@ -1,6 +1,12 @@
 angular.module('app')
-    .controller('MangerController', function($scope) {
+    .controller('MangerController', function($scope, mangerService) {
 
+      $scope.aliments = [];
 
+        mangerService.findAll().then(function(res){
+          $scope.aliments = res.data;
+        }, function (err) {
+          // oups
+        });
 
     });
--- app/js/services/mangerService.js
+++ app/js/services/mangerService.js
@@ -2,10 +2,10 @@ angular.module('app')
     .service('mangerService', function($http) {
         return {
             getAll: function() {
-                return $http.get('/users');
+                return $http.get('/fichesAliments');
             },
             update: function(id, user) {
-                return $http.put('/users/' + id, user);
+                return $http.put('/fichesAliments');
             },
 
         };
