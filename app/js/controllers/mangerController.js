c0ea4b6c75a2bd58e14fb6855c875eefc2000f16
peche56 <c.filleul56@gmail.com>
2017-04-25 13:44:31 +0200

    set var for aliments

--- app/js/controllers/mangerController.js
+++ app/js/controllers/mangerController.js
@@ -5,6 +5,9 @@ angular.module('app')
 
         mangerService.findAll().then(function(res){
           $scope.aliments = res.data;
+          $scope.cattegorie = $scope.aliments[categorie];
+          $scope.nom = $scope.aliments[nom];
+          $scope.description = $scope.aliments[description];
         }, function (err) {
           // oups
         });
