49c65c7b9416ebdddf1d0962ac3581e54bcc8982
peche56 <c.filleul56@gmail.com>
2017-04-26 15:09:58 +0200

    btn ok

--- app/js/controllers/gouterController.js
+++ app/js/controllers/gouterController.js
@@ -1,4 +1,11 @@
 angular.module('app')
     .controller('GouterController', function($scope) {
+      $scope.likeOrNot = false;
+      $scope.reessayer = true;
+
+      $scope.reessayerAlim = function() {
+        $scope.likeOrNot = false;
+        $scope.reessayer = true;
+      };
 
   });
