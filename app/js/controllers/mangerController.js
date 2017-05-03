85b79ac83768febb6ec46ee202031613cbd49ae0
peche56 <c.filleul56@gmail.com>
2017-05-03 15:26:02 +0200

    need change factoty

--- app/js/controllers/mangerController.js
+++ app/js/controllers/mangerController.js
@@ -4,6 +4,7 @@ angular.module('app')
 $scope.categories = FoodFactory;
 
   console.log($scope.categories);
+  console.log($scope.categories.feculents);
             // FoodFactory.get().then(function(res) {
             //         $scope.food = (res.data); console.log($scope.food);
             //         $scope.categories.push($scope.food);
