c0981ada68287652672c1bd9abba41ce523e3c85
peche56 <c.filleul56@gmail.com>
2017-05-03 11:10:39 +0200

    mangerFactory imported into manger controller

--- app/js/controllers/mangerController.js
+++ app/js/controllers/mangerController.js
@@ -1,6 +1,11 @@
 angular.module('app')
-    .controller('MangerController', function($scope) {
+    .controller('MangerController', function($scope, FoodFactory) {
 
+$scope.categories = FoodFactory;
 
+  console.log($scope.categories);
+            // FoodFactory.get().then(function(res) {
+            //         $scope.food = (res.data); console.log($scope.food);
+            //         $scope.categories.push($scope.food);
 
-    });
+            });
