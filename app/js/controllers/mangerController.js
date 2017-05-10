0f72d873574e2b31e5202688a8e8976e35aa6567
peche56 <c.filleul56@gmail.com>
2017-05-10 13:20:51 +0200

    object of food eaten ok

--- app/js/controllers/mangerController.js
+++ app/js/controllers/mangerController.js
@@ -1,38 +1,37 @@
 angular.module('app')
     .controller('MangerController', function($scope, FoodFactory) {
-      var i = 0;
+        var i = 0;
+        $scope.AlimentsEatens = [];
+        $scope.categories = FoodFactory;
 
-      $scope.categories = FoodFactory;
 
 
-
-      $scope.currentCategorie = $scope.categories[i].categorie;
-      $scope.currentsAliments = $scope.categories[i].aliments;
-      console.log($scope.currentsAliments);
-      $scope.nextCategorie = function(){
-        i++;
         $scope.currentCategorie = $scope.categories[i].categorie;
         $scope.currentsAliments = $scope.categories[i].aliments;
-
-      };
-      $scope.prevCategorie = function(){
-        i--;
-        $scope.currentCategorie = $scope.categories[i].categorie;
-        $scope.currentsAliments = $scope.categories[i].aliments;
-      };
-
-
-$scope.myFunct = function(index){
-  console.log(index);
-  var alreadyEaten = {
-    nameFood : $scope.currentsAliments[index].name,
-    countVote : [true, true, true],
-  };
-  console.log(alreadyEaten);
-};
-
-      $scope.selectFood = function (){
-
         console.log($scope.currentsAliments);
-      };
-  });
+        $scope.nextCategorie = function() {
+            i++;
+            $scope.currentCategorie = $scope.categories[i].categorie;
+            $scope.currentsAliments = $scope.categories[i].aliments;
+
+        };
+        $scope.prevCategorie = function() {
+            i--;
+            $scope.currentCategorie = $scope.categories[i].categorie;
+            $scope.currentsAliments = $scope.categories[i].aliments;
+        };
+
+
+        $scope.myFunct = function(index) {
+            console.log(index);
+            var alreadyEaten = {
+                nameFood: $scope.currentsAliments[index].name,
+                countVote: [true, true, true],
+            };
+            $scope.AlimentsEatens.push(alreadyEaten);
+            console.log(alreadyEaten);
+            console.log($scope.AlimentsEatens);
+        };
+
+
+    });
