c2a2733d0c35ad7f863f0acb8855440497abfb1b
peche56 <c.filleul56@gmail.com>
2017-05-16 14:35:23 +0200

    new var names

--- app/js/controllers/mangerController.js
+++ app/js/controllers/mangerController.js
@@ -2,15 +2,26 @@ angular.module('app')
     .controller('MangerController', function($scope, FoodFactory, MangerService) {
         $scope.isActive = false;
         var isSelected = false;
-          var selectedCat=false;
+        var selectedCat = false;
         var i = 0;
-        $scope.AlimentsInCatCurrent = [];
+        $scope.alimentsForDataBase = [];
         $scope.AlimentsInCat = [];
-        $scope['AlimentsInCat' + i] = [];
-
-        $scope.AlimentsInCatCurrent = $scope['AlimentsInCat' + i];
-        console.log($scope.AlimentsInCatCurrent);
         $scope.categories = FoodFactory;
+
+        var readyToAdd = true;
+        var countAlimSelected = 0;
+        for (var m = 0; m < $scope.alimentsForDataBase.length; m++) {
+            if ($scope.alimentsForDataBase[m].categorie === $scope.currentCategorie) {
+                countAlimSelected = countAlimSelected + 1;
+
+            }
+            if ($scope.currentCategorie.length != countAlimSelected || $scope.alimentsForDataBase.length === 0) {
+                readyToAdd = false;
+            }
+        }
+
+
+
 
         $scope.currentCategorie = $scope.categories[i].categorie;
         $scope.currentsAliments = $scope.categories[i].aliments;
@@ -27,7 +38,7 @@ angular.module('app')
         };
 
         var foodIndex = function(food) {
-            return $scope.AlimentsInCatCurrent.map(function(aliment) {
+            return $scope.alimentsForDataBase.map(function(aliment) {
                 return aliment.nameFood;
             }).indexOf(food.name);
         };
@@ -37,56 +48,52 @@ angular.module('app')
 
         $scope.selectFood = function(food) {
             if ($scope.isSelected(food)) {
-                $scope.AlimentsInCatCurrent.splice(foodIndex(food), 1);
-                console.log("isSelected", $scope.AlimentsInCatCurrent);
+                $scope.alimentsForDataBase.splice(foodIndex(food), 1);
             } else {
                 var alreadyEaten = {
                     categorie: $scope.currentCategorie,
                     nameFood: food.name,
                     countVote: [true, true, true],
                 };
-                $scope.AlimentsInCatCurrent.push(alreadyEaten);
-                console.log("!isSelected", $scope.AlimentsInCatCurrent);
+                $scope.alimentsForDataBase.push(alreadyEaten);
+                console.log(countAlimSelected);
+                console.log($scope.alimentsForDataBase[m].categorie);
+                console.log($scope.currentCategorie);
             }
         };
-
-        // var categorieIndex = function(categorie) {
-        //     return $scope.AlimentsInCatCurrent.map(function(aliment) {
-        //         return aliment.categorie;
-        //     }).indexOf($scope.currentCategorie);
-        // };
-        // $scope.isSelectedCat = function(categorie) {
-        //     return categorieIndex(categorie) != -1;
-        // };
+        var categorieIndex = function(categorie) {
+            return $scope.alimentsForDataBase.map(function(aliment) {
+                return aliment.nameFood;
+            }).indexOf(food.name);
+        };
+        $scope.isSelected = function(food) {
+            return foodIndex(food) != -1;
+        };
 
         $scope.selectAll = function() {
-
-            if (selectedCat === false) {
-              console.log("coucou");
-                for (j = 0; j < $scope.currentsAliments.length; j++) {
-
+            if (readyToAdd === true) {
+                for (var j = 0; j < $scope.currentsAliments.length; j++) {
+                    for (var l = 0; l < $scope.alimentsForDataBase.length; l++) {
+                        if ($scope.alimentsForDataBase[l].nameFood === $scope.currentsAliments[j].name) {
+                            $scope.alimentsForDataBase.splice($scope.currentsAliments[j].name);
+                        }
+                    }
                     var alreadyEaten = {
                         categorie: $scope.currentCategorie,
                         nameFood: $scope.categories[i].aliments[j].name,
                         countVote: [true, true, true],
                     };
-                    $scope.AlimentsInCatCurrent.push(alreadyEaten);
-                }
-                console.log($scope.AlimentsInCatCurrent);
-                selectedCat = true;
-                console.log(selectedCat);
-            } else {
-                for (var k = 0; k < $scope.AlimentsInCatCurrent.length; k++) {
-                  console.log($scope.AlimentsInCatCurrent.length);
-                    var currentAlimCat = $scope.AlimentsInCatCurrent[k].categorie;
+                    $scope.alimentsForDataBase.push(alreadyEaten);
+                    console.log("add", $scope.alimentsForDataBase);
 
-                    if (currentAlimCat === $scope.currentCategorie) {
-                        $scope.AlimentsInCatCurrent.splice(foodIndex([k]), 1);
+                }
+            } else {
+                for (var k = 0; k < $scope.alimentsForDataBase.length; k++) {
+                    if ($scope.currentCategorie === $scope.alimentsForDataBase[k].categorie) {
+                        $scope.alimentsForDataBase.splice($scope.alimentsForDataBase[k]);
+                        console.log("delete", $scope.alimentsForDataBase);
                     }
                 }
-                selectedCat = false;
-                console.log( $scope.AlimentsInCatCurrent);
-
             }
         };
 
--- app/js/factories/foodFactory.js
+++ app/js/factories/foodFactory.js
@@ -1,94 +1,101 @@
 angular.module('app')
     .factory('FoodFactory', function() {
 
-      return [
-        {
-          categorie: "Fruits",
-          portion: "Comme tous les fruits et légumes, il faut en manger au moins 5 portions par jour.",
-          aliments: [{
-              name: "Orange",
-              picto: "/img/orange.svg",
-              description: "Fruit de l'oranger. Il en existe de plusieurs variétées, couleurs et goûts.",
-              suggestions: ["crue", "salade", "jus", "gâteau", "smoothie", "confiture"],
-              composition: [""]
+        return [{
+                categorie: "Fruits",
+                portion: "Comme tous les fruits et légumes, il faut en manger au moins 5 portions par jour.",
+                aliments: [{
+                        name: "Orange",
+                        picto: "/img/orange.svg",
+                        description: "Fruit de l'oranger. Il en existe de plusieurs variétées, couleurs et goûts.",
+                        suggestions: ["crue", "salade", "jus", "gâteau", "smoothie", "confiture"],
+                        composition: [""]
+                    },
+                    {
+                        name: "Banane",
+                        picto: "/img/banana.svg",
+                        description: "C'est très bon les singes les adorent",
+                        suggestions: ["crue", "salade", "jus", "gâteau", "smoothie", "confiture"],
+                        composition: [""]
+                    }
+                ]
             },
             {
-              name: "Banane",
-              picto: "/img/banana.svg",
-              description: "C'est très bon les singes les adorent",
-              suggestions: ["crue", "salade", "jus", "gâteau", "smoothie", "confiture"],
-              composition: [""]
+                categorie: "Légumes",
+                portion: "Comme tous les fruits et légumes, il faut en manger au moins 5 portions par jour.",
+                aliments: [{
+                    name: "Carotte",
+                    picto: "/img/carrot.svg",
+                    description: "Les lapins en raffolent, ça rend aimable et donne les fesses roses.",
+                    suggestions: ["crue", "cuit", "salade", "soupe", "gâteau", "purée", "vapeur"],
+                    composition: [""]
+                }, ]
+            },
+            {
+                categorie: "Féculents",
+                portion: "Il faut manger des féculents plusieurs fois par jour sous différentes formes (pain, pâtes, pommes de terre).",
+                aliments: [{
+                        name: "Riz",
+                        picto: "/img/rice.svg",
+                        description: "On en mange beaucoup en Asie. Il en existe de multiples variétées.",
+                        suggestions: ["cuit", "gâteau", "vapeur"],
+                        composition: [""]
+                    },
+                    {
+                        name: "Pain",
+                        picto: "/img/bread.svg",
+                        description: "Aux céréales, nature, en boule… Il en existe de toutes les sortes !",
+                        suggestions: ["nature", "grillé", "avec de la confiture", "avec du beurre"],
+                        composition: ["Gluten"]
+                    },
+                ]
+            },
+            {
+                categorie: "Viandes, poissons, œufs",
+                portion: "Il faut en manger 1 fois par jour, 2 maximum.",
+                aliments: [{
+                    name: "Poisson",
+                    picto: "/img/fish.svg",
+                    description: "De mer ou de rivière, il existe une quantité impressionnante d'espèces.",
+                    suggestions: ["cuit", "crue"],
+                    composition: ["Poisson"]
+                }, {
+                    name: "Viande",
+                    picto: "/img/steak.svg",
+                    description: "Viande rouge ou viande blanche, il y en a pour tous les goûts !",
+                    suggestions: ["cuite", "en tartare", "chaude", "froide"],
+                    composition: ["Viande"]
+                }, {
+                    name: "Œuf",
+                    picto: "/img/egg.svg",
+                    description: "On mange le plus souvent ceux pondus par les poules.",
+                    suggestions: ["cocotte", "à la coque", "dur", "sur le plat", "mimosa"],
+                    composition: ["Œuf"]
+                }, ]
+            },
+            {
+                categorie: "Produits laitiers",
+                portion: "Il faut manger un produit laitier à chaque repas.",
+                aliments: [{
+                    name: "Lait",
+                    picto: "/img/milk.svg",
+                    description: "Provient le plus souvent de la vache",
+                    suggestions: ["frais", "chaud"],
+                    composition: [""]
+                }, ]
+            },
+            {
+                categorie: "Produits sucrés",
+                portion: "Il faut en consommer avec modération.",
+                aliments: [{
+                    name: "Gâteau",
+                    picto: "/img/cake.svg",
+                    description: "Aux fruits, chocolats crème... il y a l'embarras du choix.",
+                    suggestions: ["gâteau"],
+                    composition: [""]
+                }, ]
             }
-          ]
-        },
-        {
-          categorie: "Légumes",
-          portion: "Comme tous les fruits et légumes, il faut en manger au moins 5 portions par jour.",
-          aliments: [{
-              name: "Carotte",
-              picto: "/img/carrot.svg",
-              description: "Les lapins en raffolent, ça rend aimable et donne les fesses roses.",
-              suggestions: ["crue", "cuit", "salade", "soupe", "gâteau", "purée", "vapeur"],
-              composition: [""]
-            },
-          ]
-        },
-        {
-          categorie: "Féculents",
-          portion: "Il faut manger des féculents plusieurs fois par jour sous différentes formes (pain, pâtes, pommes de terre).",
-          aliments: [{
-              name: "Riz",
-              picto: "/img/rice.svg",
-              description: "On en mange beaucoup en Asie. Il en existe de multiples variétées.",
-              suggestions: ["cuit","gâteau", "vapeur"],
-              composition: [""]
-            },
-            {
-              name: "Pain",
-              picto: "/img/bread.svg",
-              description: "Aux céréales, nature, en boule… Il en existe de toutes les sortes !",
-              suggestions: ["nature","grillé", "avec de la confiture", "avec du beurre"],
-              composition: ["Gluten"]
-            },
-          ]
-        },
-        {
-          categorie: "Viandes, poissons, œufs",
-          portion: "Il faut en manger 1 fois par jour, 2 maximum.",
-          aliments: [{
-              name: "Poisson",
-              picto: "/img/fish.svg",
-              description: "De mer ou de rivière, il existe une quantité impressionnante d'espèces.",
-              suggestions: ["cuit", "crue"],
-              composition: [""]
-            },
-          ]
-        },
-        {
-          categorie: "Produits laitiers",
-          portion: "Il faut manger un produit laitier à chaque repas.",
-          aliments: [{
-              name: "Lait",
-              picto: "/img/milk.svg",
-              description: "Provient le plus souvent de la vache",
-              suggestions: ["frais", "chaud"],
-              composition: [""]
-            },
-          ]
-        },
-        {
-          categorie: "Produits sucrés",
-          portion: "Il faut en consommer avec modération.",
-          aliments: [{
-              name: "Gâteau",
-              picto: "/img/cake.svg",
-              description: "Aux fruits, chocolats crème... il y a l'embarras du choix.",
-              suggestions: ["gâteau"],
-              composition: [""]
-            },
-          ]
-        }
 
-      ];
+        ];
 
     });
