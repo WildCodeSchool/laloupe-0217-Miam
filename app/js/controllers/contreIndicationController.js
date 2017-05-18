01ce859b96aec62ae1bba8120f2667335075953b
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-05-18 20:52:07 +0200

    Try calling food with input

--- app/js/controllers/contreIndicationController.js
+++ app/js/controllers/contreIndicationController.js
@@ -11,4 +11,20 @@ angular.module('app')
 
             // $scope.counters = null;
 
+            // var categories = ;
+            $scope.currentCategorie = categorie[0];
+
+            $scope.onInput = function() {
+              var val = document.getElementById("input").value;
+              // var opts = document.getElementById("counters").childNodes;
+              // console.log(opts);
+              for (var i = 0; i < val.length; i++) {
+                if (val !== 0) {
+                  console.log(val);
+                  console.log($scope.foodList);
+                  break;
+                }
+              }
+            };
+
           });
