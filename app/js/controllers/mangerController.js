b051aec34f7ec972ae711e2862041c53bdfc0f4a
peche56 <c.filleul56@gmail.com>
2017-05-18 14:48:01 +0200

    ng-class and data object ok for single selection

--- app/js/controllers/mangerController.js
+++ app/js/controllers/mangerController.js
@@ -6,10 +6,63 @@ angular.module('app')
         console.log(Object.keys($scope.foodList));
         var categories = Object.keys($scope.foodList);
         var i = 0;
+        var j = 0;
+        var k = 0;
         $scope.filterLimit = 3;
-        $scope.currentAliments = $scope.foodList;
-        console.log($scope.currentAliments);
         $scope.currentCategorie = categories[0];
+        $scope.currentAliments = $scope.foodList[$scope.currentCategorie].aliments;
+        console.log($scope.currentAliments);
+        $scope.alimentsForDatabase = [];
+
+        var foodIndex = function(food) {
+            return $scope.alimentsForDatabase.map(function(aliment) {
+                return aliment.nameFood;
+            }).indexOf(food.name);
+        };
+
+        $scope.isSelected = function(food) {
+            return foodIndex(food) != -1;
+        };
+
+        $scope.selectFood = function(food) {
+            if ($scope.isSelected(food)) {
+                $scope.alimentsForDatabase.splice(foodIndex(food), 1);
+                console.log($scope.alimentsForDatabase);
+                console.log(foodIndex(food));
+            } else {
+                var alreadyEaten = {
+                    nameFood: food.name,
+                    countVote: [true, true, true],
+                };
+                $scope.alimentsForDatabase.push(alreadyEaten);
+                console.log($scope.alimentsForDatabase);
+            }
+        };
+
+
+        // $scope.selectFood = function(food) {
+        //
+        //     var alreadyEaten = {
+        //         nameFood: food.name,
+        //         countVote: [true, true, true],
+        //     };
+        //     console.log($scope.alimentsForDatabase.length);
+        //
+        //     if ($scope.alimentsForDatabase.length === 0) {
+        //         $scope.alimentsForDatabase.push(alreadyEaten);
+        //         console.log("add === 0", $scope.alimentsForDatabase);
+        //     } else {
+        //         for (j = 0; j < $scope.alimentsForDatabase.length; j++) {
+        //             if (food.name === $scope.alimentsForDatabase[j].nameFood) {
+        //               $scope.alimentsForDatabase.splice($scope.alimentsForDatabase[j], 1);
+        //               console.log("deleted", $scope.alimentsForDatabase);
+        //             } else {
+        //               $scope.alimentsForDatabase.push(alreadyEaten);
+        //               console.log("add in loop", $scope.alimentsForDatabase);
+        //             }
+        //         }
+        //     }
+        // };
 
 
         $scope.nextCategorie = function() {
