81a1823792da838bd3767cad1facc2ccf6c544b1
Prakamsay28170 <prakamsay28@gmail.com>
2017-05-24 13:56:39 +0200

    objectif in reward factory

--- app/js/factories/rewardFactory.js
+++ app/js/factories/rewardFactory.js
@@ -4,37 +4,37 @@ angular.module('app')
         var reward = [{
             picto: "",
             name: "",
-            objectif: "Bravo ! Tu as goûté cet aliment."
+            objectif: "Bravo ! Tu as goûté 1 fois cet aliment !"
           },
           {
             picto: "",
             name: "",
-            objectif: "Bravo ! Tu as goûté 2 aliments"
+            objectif: "Bravo ! Tu as goûté 3 fois cet aliment !"
           },
           {
             picto: "",
             name: "",
-            objectif: ""
+            objectif: "Bravo ! Tu as aimé 2 aliments !"
           },
           {
             picto: "",
             name: "",
-            objectif: ""
+            objectif: "Bravo ! Tu as aimé 3 aliments !"
           },
           {
             picto: "",
             name: "",
-            objectif: ""
+            objectif: "Bravo ! Tu as aimé 5 aliments !"
           },
           {
             picto: "",
             name: "",
-            objectif: ""
+            objectif: "Waouh ! Bravo ! Tu as aimé tous les aliments de la même famille!"
           },
           {
             picto: "",
             name: "",
-            objectif: ""
+            objectif: "Félicitations ! On est fier de toi, tu as tout aimé !"
           },
           {
             picto: "",
