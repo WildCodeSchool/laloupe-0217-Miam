628a24257e3f19228bd8ff13412b684d0a8db4d1
Prakamsay28170 <prakamsay28@gmail.com>
2017-06-15 22:02:33 +0200

    try change to dev

--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -1,5 +1,5 @@
 angular.module('app')
-    .controller('LoginController' function($scope, state, Auth) {
+    .controller('LoginController'function($scope, state, Auth) {
         $scope.errors = [];
 
         $scope.login = function() {
@@ -12,19 +12,4 @@ angular.module('app')
                 });
             }
         };
-        // $scope.remember = false;
-        //        if ($remember('username') && $remember('password') ) {
-        //            $scope.remember = true;
-        //            $scope.username = $remember('username');
-        //            $scope.password = $remember('password');
-        //        }
-        //        $scope.rememberMe = function() {
-        //            if ($scope.remember) {
-        //                $remember('username', $scope.username);
-        //                $('password', $scope.password);
-        //            } else {
-        //                $remember('username', '');
-        //                $remember('password', '');
-        //            }
-        //        };
-           });
+    });
