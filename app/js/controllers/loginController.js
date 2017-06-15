db044326a416570ac0c1fdf2991ac5d138c024d4
Prakamsay28170 <prakamsay28@gmail.com>
2017-06-15 21:55:15 +0200

    new branch

--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -1,5 +1,5 @@
 angular.module('app')
-    .controller('LoginController', 'RememberService', function($scope, $remember, $state, Auth) {
+    .controller('LoginController' function($scope, state, Auth) {
         $scope.errors = [];
 
         $scope.login = function() {
@@ -12,19 +12,19 @@ angular.module('app')
                 });
             }
         };
-        $scope.remember = false;
-               if ($remember('username') && $remember('password') ) {
-                   $scope.remember = true;
-                   $scope.username = $remember('username');
-                   $scope.password = $remember('password');
-               }
-               $scope.rememberMe = function() {
-                   if ($scope.remember) {
-                       $remember('username', $scope.username);
-                       $('password', $scope.password);
-                   } else {
-                       $remember('username', '');
-                       $remember('password', '');
-                   }
-               };
+        // $scope.remember = false;
+        //        if ($remember('username') && $remember('password') ) {
+        //            $scope.remember = true;
+        //            $scope.username = $remember('username');
+        //            $scope.password = $remember('password');
+        //        }
+        //        $scope.rememberMe = function() {
+        //            if ($scope.remember) {
+        //                $remember('username', $scope.username);
+        //                $('password', $scope.password);
+        //            } else {
+        //                $remember('username', '');
+        //                $remember('password', '');
+        //            }
+        //        };
            });
