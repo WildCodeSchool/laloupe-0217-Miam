62cd72c056107df0edec396bae2ab64722fddb21
Prakamsay28170 <prakamsay28@gmail.com>
2017-06-19 09:30:13 +0200

    début

--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -1,5 +1,5 @@
 angular.module('app')
-    .controller('LoginController'function($scope, state, Auth) {
+    .controller('LoginController', function($scope, state, ngCookies, rememberMeService, Base64, Auth) {
         $scope.errors = [];
 
         $scope.login = function() {
@@ -10,6 +10,23 @@ angular.module('app')
                 }).catch(function(err) {
                     $scope.errors.push(err);
                 });
+            }
+        };
+
+        self = this;
+
+        if (rememberMeService('7ZXYZ@L') && rememberMeService('UU@#90')) {
+            self.user.email = Base64.decode(rememberMeService('7ZXYZ@L'));
+            self.user.password = Base64.decode(rememberMeService('UU@#90'));
+        }
+        self.rememberMe = function() {
+            if (self.remember) {
+
+                rememberMeService('7ZXYZ@L', Base64.encode(self.user.email));
+                rememberMeService('UU@#90', Base64.encode(self.user.password));
+            } else {
+                rememberMeService('7ZXYZ@L', '');
+                rememberMeService('UU@#90', '');
             }
         };
     });
