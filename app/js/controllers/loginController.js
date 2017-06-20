64018ca005ded1ac3ec1163a7a9a0bbf39f850e0
Prakamsay28170 <prakamsay28@gmail.com>
2017-06-20 12:02:49 +0200

    test code

--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -1,32 +1,31 @@
 angular.module('app')
-    .controller('LoginController', function($scope, state, ngCookies, rememberMeService, Base64, Auth) {
-        $scope.errors = [];
+    .controller('LoginController', function($scope, $state, RememberMeService, Auth) {
+            $scope.errors = [];
 
-        $scope.login = function() {
-            if ($scope.loginForm.$valid) {
-                $scope.errors = [];
-                Auth.login($scope.user).then(function(result) {
-                    $state.go('anon.gouter');
-                }).catch(function(err) {
-                    $scope.errors.push(err);
-                });
-            }
-        };
+            $scope.login = function() {
+                if ($scope.loginForm.$valid) {
+                    $scope.errors = [];
+                    Auth.login($scope.user).then(function(result) {
+                        $state.go('anon.gouter');
+                    }).catch(function(err) {
+                        $scope.errors.push(err);
+                    });
+                  }
+                };
 
-        self = this;
-
-        if (rememberMeService('7ZXYZ@L') && rememberMeService('UU@#90')) {
-            self.user.email = Base64.decode(rememberMeService('7ZXYZ@L'));
-            self.user.password = Base64.decode(rememberMeService('UU@#90'));
-        }
-        self.rememberMe = function() {
-            if (self.remember) {
-
-                rememberMeService('7ZXYZ@L', Base64.encode(self.user.email));
-                rememberMeService('UU@#90', Base64.encode(self.user.password));
-            } else {
-                rememberMeService('7ZXYZ@L', '');
-                rememberMeService('UU@#90', '');
-            }
-        };
-    });
+                $scope.remember = false;
+                if ($remember('email') && $remember('password')) {
+                  $scope.remember = true;
+                  $scope.user.email = $remember('email');
+                  $scope.user.password = $remember('password');
+                }
+                $scope.rememberMe = function () {
+                  if ($scope.remember) {
+                    $remember('email', $scope.user.email);
+                    $remember('password', $scope.user.password);
+                  } else {
+                    $remember('email', '');
+                    $remember('password', '');
+                  }
+                };
+            });
