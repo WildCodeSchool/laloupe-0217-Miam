74b169b9c8a420dd2185e9f290128dc68dca9498
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-07-10 13:41:14 +0200

    Improve code presentation

--- app/js/services/AvatarService.js
+++ app/js/services/AvatarService.js
@@ -1,7 +1,6 @@
 angular.module('app')
     .service('AvatarService', function($http) {
         return {
-
             findOneAndUpdateProfil: function(userId, name, nameAvatar) {
                 console.log(userId, name, nameAvatar);
                 return $http.post('/profiles/updateProfil', {user: userId, profil: {userName: name, nameAvatar: nameAvatar}});
--- app/js/services/ProfilService.js
+++ app/js/services/ProfilService.js
@@ -1,20 +1,16 @@
 angular.module('app')
     .service('ProfilService', function($http) {
         return {
-
             findOneAndUpdateName: function(user, isCurrentProfil, name, nameAvatar){
               console.log(user, isCurrentProfil, name, nameAvatar);
               return $http.post('/profiles/updateProfil', { user: user, isCurrentProfil: isCurrentProfil, userName: name, nameAvatar: nameAvatar });
             },
-
             getAll: function() {
-                          return $http.get('/profiles');
-                      },
-
+              return $http.get('/profiles');
+            },
             changeProfil: function(changeProfil){
               console.log('changeProfil', changeProfil);
               return $http.post('/profiles/changeProfil', changeProfil);
             },
-
         };
 });
