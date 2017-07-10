9ec78d6991db4cb099f1d2b15f61bf7d050767a6
Cendrine Buisson <cendrine.buisson@gmail.com>
2017-07-10 13:47:49 +0200

    Call factories with return

--- app/js/factories/AvatarFactory.js
+++ app/js/factories/AvatarFactory.js
@@ -1,70 +1,59 @@
-angular.module('app')
-    .factory('AvatarFactory', function() {
-        var avatarList = [{
-                famille: "",
-                avatars: [{
-                        name: "penguin",
-                        picto: "/img/penguin.svg"
-                    },
-                    {
-                        name: "",
-                        picto: ""
-                    },
-                    {
-                        name: "",
-                        picto: ""
-                    }
-                ]
-            },
-            {
-                famille: "",
-                avatars: [{
-                        name: "lion",
-                        picto: "/img/lion.svg"
-                    },
-                    {
-                        name: "lion red",
-                        picto: "/img/lion-red.svg"
-                    },
-                    {
-                        name: "lion green",
-                        picto: "/img/lion-green.svg"
-                    },
-                    {
-                        name: "lion orange",
-                        picto: "/img/lion-orange.svg"
-                    },
-                    {
-                        name: "lion indian",
-                        picto: "/img/lion-indian.svg"
-                    }
-                ]
-            },
-             {
-                famille: "",
-                avatars: [{
-                        name: "cat",
-                        picto: "/img/cat.svg"
-                    },
-                    {
-                        name: "cat red",
-                        picto: "/img/cat-red.svg"
-                    },
-                    {
-                        name: "cat green",
-                        picto: "/img/cat-red.svg"
-                    },
-                    {
-                        name: "cat orange",
-                        picto: "/img/cat-orange.svg"
-                    },
-                    {
-                        name: "cat indian",
-                        picto: "/img/cat-indian.svg"
-                    }
-                ]
-            }
-
-        ];
-        return avatarList;
-    });
+angular.module('app').factory('AvatarFactory', function() {
+  return [
+    {
+      famille: "",
+      avatars: [
+        {
+          name: "penguin",
+          picto: "/img/penguin.svg"
+        }, {
+          name: "",
+          picto: ""
+        }, {
+          name: "",
+          picto: ""
+        }
+      ]
+    }, {
+      famille: "",
+      avatars: [
+        {
+          name: "lion",
+          picto: "/img/lion.svg"
+        }, {
+          name: "lion red",
+          picto: "/img/lion-red.svg"
+        }, {
+          name: "lion green",
+          picto: "/img/lion-green.svg"
+        }, {
+          name: "lion orange",
+          picto: "/img/lion-orange.svg"
+        }, {
+          name: "lion indian",
+          picto: "/img/lion-indian.svg"
+        }
+      ]
+    }, {
+      famille: "",
+      avatars: [
+        {
+          name: "cat",
+          picto: "/img/cat.svg"
+        }, {
+          name: "cat red",
+          picto: "/img/cat-red.svg"
+        }, {
+          name: "cat green",
+          picto: "/img/cat-red.svg"
+        }, {
+          name: "cat orange",
+          picto: "/img/cat-orange.svg"
+        }, {
+          name: "cat indian",
+          picto: "/img/cat-indian.svg"
+        }
+      ]
+    }
+  ];
+});
--- app/js/factories/rewardFactory.js
+++ app/js/factories/rewardFactory.js
@@ -1,62 +1,41 @@
-angular.module('app')
-    .factory('RewardFactory', function() {
-
-        var rewardList = [{
-            picto: "/img/university.svg",
-            name: "Découverte",
-            objectif: "Bravo ! Tu as goûté 1 fois cet aliment !"
-          },
-          {
-            picto: "/img/indian.svg",
-            name: "Petit goûteur",
-            objectif: "Bravo ! Tu as goûté 3 fois cet aliment !"
-          },
-          {
-            picto: "",
-            name: "Goûteur",
-            objectif: "Bravo ! Tu as aimé 3 aliments !"
-          },
-
-          {
-            picto: "/img/red.svg",
-            name: "Géant rouge",
-            objectif: "Bravo ! Tu as aimé les aliments rouges !"
-          },
-          {
-            picto: "/img/orange-avatar.svg",
-            name: "Géant orange",
-            objectif: "Bravo ! Tu as aimé les aliments oranges !"
-          },
-          {
-            picto: "/img/green.svg",
-            name: "Géant vert",
-            objectif: "Bravo ! Tu as aimé les aliments verts !"
-          },
-          {
-            picto: "",
-            name: "Gourmet",
-            objectif: "Waouh ! Bravo ! Tu as aimé tous les aliments de la même famille!"
-          },
-          {
-            picto: "",
-            name: "Fin gourmet",
-            objectif: "Waouh ! Bravo ! Tu as aimé tous les aliments de 3 famille!"
-          },
-          {
-            picto: "",
-            name: "Gastronome",
-            objectif: "Félicitations ! On est fier de toi, tu as tout goûté !"
-          },
-
-        ];
-        return rewardList;
-        // return {
-        //     user: function() {
-        //         if (LocalService.get('user')) {
-        //             return angular.fromJson(LocalService.get('user'));
-        //         } else {
-        //             return {};
-        //         }
-        //     }
-        // };
-    });
+angular.module('app').factory('RewardFactory', function() {
+  return [
+    {
+      picto: "/img/university.svg",
+      name: "Découverte",
+      objectif: "Bravo ! Tu as goûté 1 fois cet aliment !"
+    }, {
+      picto: "/img/indian.svg",
+      name: "Petit goûteur",
+      objectif: "Bravo ! Tu as goûté 3 fois cet aliment !"
+    }, {
+      picto: "",
+      name: "Goûteur",
+      objectif: "Bravo ! Tu as aimé 3 aliments !"
+    }, {
+      picto: "/img/red.svg",
+      name: "Géant rouge",
+      objectif: "Bravo ! Tu as aimé les aliments rouges !"
+    }, {
+      picto: "/img/orange-avatar.svg",
+      name: "Géant orange",
+      objectif: "Bravo ! Tu as aimé les aliments oranges !"
+    }, {
+      picto: "/img/green.svg",
+      name: "Géant vert",
+      objectif: "Bravo ! Tu as aimé les aliments verts !"
+    }, {
+      picto: "",
+      name: "Gourmet",
+      objectif: "Waouh ! Bravo ! Tu as aimé tous les aliments de la même famille!"
+    }, {
+      picto: "",
+      name: "Fin gourmet",
+      objectif: "Waouh ! Bravo ! Tu as aimé tous les aliments de 3 famille!"
+    }, {
+      picto: "",
+      name: "Gastronome",
+      objectif: "Félicitations ! On est fier de toi, tu as tout goûté !"
+    }
+  ];
+});
